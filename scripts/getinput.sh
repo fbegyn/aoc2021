00c98224d5f7aa4ab5efba0301e7c9f898da8ef3
Francis Begyn <francis@begyn.be>
2021-12-02 20:39:53 +0100

    inputs - fix scritps and include

--- scripts/getinput.sh
+++ scripts/getinput.sh
@@ -5,7 +5,12 @@ DIR=day$(printf "%02d" ${DAY})
 
 mkdir -p ./inputs/${DIR}/
 
-curl \
+curl "https://adventofcode.com/2021/day/${DAY}/input" \
+  -H 'authority: adventofcode.com' \
+  -H 'pragma: no-cache' \
+  -H 'cache-control: no-cache' \
+  -H 'upgrade-insecure-requests: 1' \
+  -H "referer: https://adventofcode.com/2021/day/${DAY}" \
+  -H "cookie: ${AOCCOOKIE}" \
+  --compressed \
   -fSL -o ./inputs/${DIR}/input.txt \
-  -H "Cookie: ${AOCCOOKIE}" \
-  https://adventofcode.com/2020/day/${DAY}/input
